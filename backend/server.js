730c472f9172fc9bef0761b6fc80e5e517b20606
Dirane <diranengala@gmail.com>
2023-10-12 15:18:02 +0100

    Added server route and logic for login request

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection({
 })
 
 app.post('/signup', (req, res) => {
-    const sql = "INSERT INTO login('name', 'email', 'password') VALUES (?)";
+    const sql = "INSERT INTO login(`name`, `email`, `password`) VALUES (?)";
     const values = [
         req.body.name,
         req.body.email,
@@ -25,6 +25,21 @@ app.post('/signup', (req, res) => {
             return res.json("Error");
         }
         return res.json(data);
+    })
+})
+
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM login WHERE `email` = ? AND `password` = ?";
+    db.query(sql, [req.body.email, req.body.password], (err, data) => {
+        if(err){
+            return res.json("Error");
+        }
+        if(data.length > 0){
+            return res.json("Success");
+        }
+        else {
+            return res.json("Fail");
+        }
     })
 })
 
